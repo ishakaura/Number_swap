d87e69b50f0e5e9439dfb59a10a2df3afe601adc
theerthaprem13-dotcom <theerthaprem13@gmail.com>
2026-07-06 21:54:51 +0400

    number swap

---
+++ SwapingNumbers.py
@@ -0,0 +1,23 @@
+#Get user input
+num1=int(input("Enter number a:"))
+num2=int(input("Enter number b:"))
+num3=int(input("Enter number c:"))
+
+#Get the order(eg.c,b,a or c,a,b)
+order=input("Enter the order:")
+
+#Make condition
+
+if order=="a,b,c":
+    numbers=num1,num2,num3
+elif order=="c,b,a":
+    numbers=num3,num2,num1
+elif order=="c,a,b":
+    numbers=num3,num1,num2
+elif order=="a,c,b":
+    numbers=num1,num3,num2
+elif order=="b,c,a":
+    numbers=num2,num3,num1
+elif order=="b,a,c":
+    numbers=num2,num1,num3
+print(numbers)
